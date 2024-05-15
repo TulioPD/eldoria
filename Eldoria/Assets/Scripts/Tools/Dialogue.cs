81ade8cf7037104786e7a219d2f42cc4f97d95b7
TulioPD <julio.12pd@gmail.com>
2024-05-16 00:25:45 +0200

    Dialogue system refactored and new table added
    Dialogue system now works with multilanguage

--- Eldoria/Assets/Scripts/Tools/Dialogue.cs
+++ Eldoria/Assets/Scripts/Tools/Dialogue.cs
@@ -1,55 +1,75 @@
 using System.Collections;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Localization;
 
 public class Dialogue : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
-    public string[] lines;
+    public LocalizedString[] localizedLines;
     public float textSpeed;
     private int index;
     private bool isTyping = false;
-
+    private string[] lines;
+    private Coroutine typingCoroutine;
 
     private void Start()
     {
-        textComponent.text=string.Empty;
+        textComponent.text = string.Empty;
+        InitializeLocalizedLines();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)){
-            if (textComponent.text == lines[index])
-            {
-                NextLine();
-            }
-            else
-            {
-                StopAllCoroutines();
-                textComponent.text = lines[index];
-            }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            HandleDialogueInput();
         }
     }
+
     public void StartDialogue()
     {
         index = 0;
         textComponent.text = string.Empty;
-        StartCoroutine(TypeLine());
+        StartTypingLine();
     }
 
     public void EndDialogue()
     {
-        StopAllCoroutines();
+        StopTyping();
+        textComponent.text = string.Empty;
+    }
+
+    private void StartTypingLine()
+    {
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+        }
+        typingCoroutine = StartCoroutine(TypeLine());
+    }
+
+    private void StopTyping()
+    {
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+            typingCoroutine = null;
+        }
+        isTyping = false;
     }
 
     private IEnumerator TypeLine()
     {
         isTyping = true;
-        foreach (char c in lines[index].ToCharArray())
+        textComponent.text = string.Empty;
+
+        foreach (char c in lines[index])
         {
             textComponent.text += c;
-            yield return new WaitForSeconds(textSpeed);
+            yield return TimerManager.Instance.StartTimer(textSpeed, null);
         }
+
         isTyping = false;
     }
 
@@ -58,12 +78,36 @@ public class Dialogue : MonoBehaviour
         if (index < lines.Length - 1)
         {
             index++;
-            textComponent.text = string.Empty;
-            StartCoroutine(TypeLine());
+            StartTypingLine();
         }
         else
         {
             EndDialogue();
+        }
+    }
+
+    private void InitializeLocalizedLines()
+    {
+        lines = new string[localizedLines.Length];
+
+        for (int i = 0; i < localizedLines.Length; i++)
+        {
+            int indexCopy = i;
+            localizedLines[i].StringChanged += (localizedString) => lines[indexCopy] = localizedString;
+            lines[i] = localizedLines[i].GetLocalizedString();
+        }
+    }
+
+    private void HandleDialogueInput()
+    {
+        if (isTyping)
+        {
+            StopTyping();
+            textComponent.text = lines[index];
+        }
+        else if (textComponent.text == lines[index])
+        {
+            NextLine();
         }
     }
 }
